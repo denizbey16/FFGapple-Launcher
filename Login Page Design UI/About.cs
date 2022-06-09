d4e19dd737f13d194ba36dbb56721ff761e45264
FFGapple <107075777+ffgapple@users.noreply.github.com>
2022-06-09 07:37:46 +0200

    Revert "Cleanup"

---
+++ Login Page Design UI/About.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFGapple
+{
+    public partial class About : Form
+    {
+        int Move;
+        int Mouse_X;
+        int Mouse_Y;
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void About_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void About_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void About_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void About_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            Move = 0;
+        }
+
+        private void About_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (Move == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - Mouse_X, MousePosition.Y - Mouse_Y);
+            }
+        }
+
+        private void About_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            Move = 1;
+            Mouse_X = e.X;
+            Mouse_Y = e.Y;
+        }
+    }
+}
--- Login Page Design UI/Program.cs
+++ Login Page Design UI/Program.cs
@@ -1,18 +1,24 @@
-﻿#region
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-using FFGapple.Forms;
-
-#endregion
-
-namespace FFGapple;
-
-internal static class Program
+namespace Login_Page_Design_UI
 {
-    [STAThread]
-    private static void Main()
+    static class Program
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new Giris());
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+           
+            Application.Run(new Giris());
+        }
     }
 }
